fc6c5936c38d77ade7914941307c7957852b2b78
leodomingue <leodomingue2016@gmail.com>
2025-01-13 23:40:09 -0300

    Test2  del bot NeopiaBot

--- bot/index.js
+++ bot/index.js
@@ -2,7 +2,13 @@
 const { Client, GatewayIntentBits, Events } = require("discord.js"); // Importación correcta
 
 // Crear cliente de discord y permisos
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+  intents: [
+      GatewayIntentBits.Guilds,          // Para eventos de servidor
+      GatewayIntentBits.GuildMessages,  // Para escuchar mensajes en los canales
+      GatewayIntentBits.MessageContent  // Para acceder al contenido de los mensajes
+  ]
+});
 
 // Cuando el cliente esté listo, ejecuta este código (solo una vez).
 client.once(Events.ClientReady, readyClient => {
@@ -19,4 +25,4 @@ client.on('messageCreate', (message) => {
 
 
 // Log in to Discord with your client's token
-client.login("MTMyODUyMTMxOTUxMTQ5MDcyMw.GhyfSv.LhrGG6e5kCn0druaJj9YDjez9usrE3eWpPamUo");
+client.login("MTMyODUyMTMxOTUxMTQ5MDcyMw.GrI9gT.CtCOYh9OZCAT0_n5kd2VbbTLTCK9AHxyeiK2U4");
