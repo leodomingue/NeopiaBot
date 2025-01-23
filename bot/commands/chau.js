09a9da2066154ff31584794119732f34046b9cb2
leodomingue <leodomingue2016@gmail.com>
2025-01-23 06:30:10 -0300

    Se modifica los saludos y se crea Json con saludos

--- bot/commands/chau.js
+++ bot/commands/chau.js
@@ -1,6 +1,21 @@
+const fs = require('fs');
+const { EmbedBuilder } = require('discord.js');
+const jsonImagesActionsNeopets = JSON.parse(fs.readFileSync('../images_actions.json', 'utf8'))
+
 module.exports = {
-    description: "Dice ¡Chau! Tonto",
+    description: "El usuario se despide de todos con una imagen random",
     run: async(message) => {
-        message.reply("¡Chau! Tonto");
+
+        let number = (Math.floor(Math.random() * jsonImagesActionsNeopets["greetings"].length))
+
+        // Crear un embed con los datos obtenidos
+        const embed = new EmbedBuilder()
+        .setColor(0x99FF00)
+        .setDescription(` **${message.author.username}** Se despide      `)
+        .setImage(jsonImagesActionsNeopets["greetings"][number])
+        .setFooter({ text: jsonImagesActionsNeopets["greetings"][number] })
+
+
+        message.channel.send({ embeds: [embed] });
     }
 }
--- bot/commands/hola.js
+++ bot/commands/hola.js
@@ -1,6 +1,21 @@
+const fs = require('fs');
+const { EmbedBuilder } = require('discord.js');
+const jsonImagesActionsNeopets = JSON.parse(fs.readFileSync('../images_actions.json', 'utf8'))
+
 module.exports = {
-    description: "Dice ¡Hola! Soy NeopiaBot",
+    description: "El usuario saluda a todos con una imagen random",
     run: async(message) => {
-        message.reply("¡Hola! Soy NeopiaBot");
+
+        let number = (Math.floor(Math.random() * jsonImagesActionsNeopets["greetings"].length))
+
+        // Crear un embed con los datos obtenidos
+        const embed = new EmbedBuilder()
+        .setColor(0x99FF00)
+        .setDescription(`🎊 **${message.author.username}** saluda a todos 🎊    `)
+        .setImage(jsonImagesActionsNeopets["greetings"][number])
+        .setFooter({ text: jsonImagesActionsNeopets["greetings"][number] })
+
+
+        message.channel.send({ embeds: [embed] });
     }
 }
